5469b5e558041c3c9c462a4220eb0596c0cdb023
Malik <malik.kouch62@gmail.com>
2024-03-29 16:15:31 +0100

    ajoute switch stp

--- exo.js
+++ exo.js
@@ -11,3 +11,15 @@ console.log("J'aurai "+age+" ans dans 5 ans.");
 const PRENOM = "Malik";
 const NOMCOMPLET = `${PRENOM} ${NOM}`;
 console.log(NOMCOMPLET)
+
+// exo4
+let nombre1 = 6;
+let nombre2 = 9;
+
+if (nombre1 == nombre2) {
+    console.log(`${nombre1} égalité avec ${nombre2}`);
+} else if (nombre1 > nombre2) {
+    console.log(`${nombre1} supérieur à ${nombre2}`);
+} else {
+    console.log("Ne rentre pas dans les conditions")
+}
---
+++ switch.js
@@ -0,0 +1,16 @@
+let couleur = ""
+
+switch (couleur) {
+    case "rouge":
+        console.log("danger");
+        break;
+    case "orange":
+            console.log("attention");
+            break;
+        case "vert":
+            console.log(fini);
+            break;
+        default:
+            console.log("jsp");
+            break;
+}
