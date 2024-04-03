9727c84b8aa0a9671dea603477467861a015e89e
Kolima62 <163014133+Kolima62@users.noreply.github.com>
2024-04-03 12:23:58 +0200

    Add files via upload

--- index.js
+++
@@ -1,8 +0,0 @@
-var leNomDeMaVariable = 45;
-let leNomDeMaDeuxiemeVariable =46;
-const TEST = 35;
-
-console.log("deuxieme variable :",leNomDeMaDeuxiemeVariable);
-console.log("premiere variable :",leNomDeMaVariable);
-console.log("RANDOM BULLSHIT GO!!!");
-console.log("constante :",TEST);
